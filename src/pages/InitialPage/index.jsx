e2e9cf8db15e270155ed79f39611622fa05d890b
BrFuentes <brayantigiza@gmail.com>
2023-02-06 21:53:51 -0500

    tamaño de descripción general

--- src/pages/InitialPage/index.jsx
+++ src/pages/InitialPage/index.jsx
@@ -15,6 +15,10 @@ function InitialPage(){
                 <p>
                 Soy un profesional con habilidades técnicas en la aplicación de principios químicos y físicos para el diseño, desarrollo y optimización de procesos, asi como en lenguajes de programación para el desarrollo de páginas web. Destaco por ser un profesional versátil, disciplinado y capaz de trabajar y colaborar en diferentes entornos, desde la investigación y el desarrollo hasta la implementación y producción.
                 </p>
+                <div className="btn-cv">
+                <a href="./CV Brayan Fuentes.pdf" download>Descargar Hoja de Vida</a>
+                </div>
+                
             </div>
         </div>
     )
